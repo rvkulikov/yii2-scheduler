9f8f33a0af14b0114668b8719f8576578015ed8c
Roman Kulikov <rkulikov@bigland.ru>
2021-07-18 23:04:28 +0300

    fix(Общее): Убрал пагинацию

--- app/backend/src/Controllers/SchedulesController.php
+++ app/backend/src/Controllers/SchedulesController.php
@@ -3,11 +3,13 @@
 namespace Rvkulikov\Yii2\Scheduler\Controllers;
 
 use Rvkulikov\Yii2\Scheduler\Components\DefinitionLocatorInterface;
+use Rvkulikov\Yii2\Scheduler\Components\SettingsLocatorInterface;
 use Rvkulikov\Yii2\Scheduler\Data\Serializer;
 use Rvkulikov\Yii2\Scheduler\Models\Job;
 use Rvkulikov\Yii2\Scheduler\Models\Schedule;
 use Rvkulikov\Yii2\Scheduler\Services\ScheduleRepository\ScheduleRepositoryInterface;
 use Yii;
+use yii\data\ArrayDataProvider;
 use yii\data\DataProviderInterface;
 use yii\rest\Controller;
 
@@ -16,10 +18,15 @@ class SchedulesController extends Controller
     public $serializer = Serializer::class;
 
     public function actionIndex(
+        SettingsLocatorInterface $settingsLocator,
         DefinitionLocatorInterface $definitionLocator,
         ScheduleRepositoryInterface $scheduleRepository
     ): DataProviderInterface
     {
+        if ($settingsLocator->getCronEnabled() === false) {
+            return new ArrayDataProvider(['allModels' => []]);
+        }
+
         $filter = Yii::createObject($definitionLocator->getScheduleFilterClass());
         $filter->setStateAlias(Schedule::STATE_ENABLED);
         $filter->setJobStateAlias(Job::STATE_ENABLED);
