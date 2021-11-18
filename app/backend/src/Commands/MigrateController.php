f8276cc2ef419a336e8402926f8de48c0aa82d17
Roman Kulikov <r.v.kulikov@yandex.ru>
2021-11-18 15:01:34 +0300

    feat: Добавлена простая миграция для схемы

--- app/backend/src/Commands/MigrateController.php
+++ app/backend/src/Commands/MigrateController.php
@@ -4,10 +4,11 @@ namespace Rvkulikov\Yii2\Scheduler\Commands;
 
 use Rvkulikov\Yii2\Scheduler\Components\ConnectionLocatorInterface;
 use yii\helpers\ArrayHelper;
+use yii\helpers\Console;
 
 class MigrateController extends \yii\console\controllers\MigrateController
 {
-    public function __construct($id, $module, ConnectionLocatorInterface $connectionLocator, $config = [])
+    public function __construct($id, $module, private ConnectionLocatorInterface $connectionLocator, $config = [])
     {
         parent::__construct($id, $module, ArrayHelper::merge([
             'db'                  => $connectionLocator->getDb(),
@@ -15,5 +16,19 @@ class MigrateController extends \yii\console\controllers\MigrateController
             'migrationPath'       => null,
             'migrationNamespaces' => ['Rvkulikov\Yii2\Scheduler\Migrations']
         ], $config));
+    }
+
+    protected function createMigrationHistoryTable()
+    {
+        $names  = $this->db->schema->schemaNames;
+        $schema = $this->connectionLocator->getSchema();
+
+        if (empty($names[$schema])) {
+            $this->stdout("Creating schema \"$schema\"...", Console::FG_YELLOW);
+            $this->db->createCommand("create schema if not exists {$schema}")->execute();
+            $this->stdout("Done.\n", Console::FG_GREEN);
+        }
+
+        parent::createMigrationHistoryTable();
     }
 }
